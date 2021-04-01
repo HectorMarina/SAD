1635d012023c4e5b9f0e69b0aeca5a3893a3af7a
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-01 21:11:17 +0200

    Update Line.java

--- Practica 1/Basic/Line.java
+++ Practica 1/Basic/Line.java
@@ -1,59 +1,119 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica1.Basic;
+package Basic;
 
-/**
- *
- * @author Marina
- */
+//import java.util.ArrayList;
 public class Line {
-    
-    String line;
-    int pos;
+
+    //private ArrayList<Character> linea;
+    private String line;
+    private int pos;
+    private boolean permission; //Variable utilizada para dar permiso a la hora de insertar un caracter
 
     public Line() {
-        line = new String();
-        pos = 0;
+        this.line = new String();
+        //this.linea = new ArrayList<>();
+        this.pos = 0;
+        this.permission = false;
     }
 
     String getLine() {
-
-        return line;
+        //Devuelve el contenido de la linea
+        return this.line;
 
     }
 
     int getPos() {
-        return pos;
+        //Devuelve la posicion actual del cursor
+        return this.pos;
     }
 
-    void insertChar() {
+    void insertChar(char character) {
+        //Miramos si tenemos permiso para sobreescribir
+        if (this.permission) {
+            if (this.pos < this.line.length()) {
+                //Si no tenemos el cursor al final de la linea insertamos en la posicion actual substituyendo por el caracter que habia antes
+                this.line = this.line.substring(0, this.pos) + Character.toString(character) + this.line.substring(this.pos + 1, this.line.length());
+            } else {
+                //Si estamos al final de la linea substituimos el caracter de la ultima posicion por el que insertamos
+                this.line = this.line.substring(0, this.line.length() - 1) + Character.toString(character);
+            }
+        } else {
+            //Si no tenemos permiso para sobreescribir insertamos el caracter en la posicion actual sin sobreescribir
+            this.line = this.line = this.line.substring(0, this.pos) + Character.toString(character) + this.line.substring(this.pos, this.line.length());
+        }
+        /*
+        //Miramos si tenemos permiso para sobreescribir
+        if (this.permission) {
+            if (this.pos < this.linea.size()) {
+                //Si no tenemos el cursor al final de la linea insertamos en la posicion actual substituyendo por el caracter que habia antes
+                this.linea = this.linea.set(this.pos, character);
+            } else {
+                //Si estamos al final de la linea substituimos el caracter de la ultima posicion por el que insertamos
+                this.linea = this.linea.add(this.pos,character);
+            }
+        } else {
+            //Si no tenemos permiso para sobreescribir insertamos el caracter en la posicion actual sin sobreescribir
+            this.linea = this.linea.add(this.pos, character) ;
+        }*/
+        this.pos++;
 
     }
 
-    void deleteChar() {
+    public void deleteChar() {
+        //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        /*if (this.pos != this.linea.size()) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.linea = this.linea.remove(this.pos);
+        }*/
+
+        //Si no me encuentro al inicio de linea procedo a borrar
+        if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.line = this.line.substring(0, this.pos) + this.line.substring(this.pos + 1, this.line.length());
+
+        }
+    }
+
+    public void bkspChar() {
+        //Si //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        /*if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.linea = this.linea.remove(this.pos - 1);
+            this.goLeft();
+        }
+         */
+        //Si //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.line = this.line.substring(0, this.pos - 1) + this.line.substring(this.pos, this.line.length());
+            this.goLeft();
+        }
 
     }
 
     void goLeft() {
-        if (pos != 0) {
-            pos--;
+        //Mueve el cursor a la izquierda
+        if (this.pos != 0) {
+            //Si el cursor no se encuentra al principio de la linea lo mueve a la izquierda
+            this.pos--;
         }
     }
 
     void goRight() {
-        if (pos != line.length()) {
-            pos++;
+        //Mueve el cursor a la derecha
+        if (this.pos != this.line.length()) {
+            //Si el cursor no se encuentra al final de la linea lo mueve a la derecha
+            this.pos++;
         }
     }
 
     void goHome() {
-        pos = 0;
+        //Mueve el cursor al inicio de la linea
+        this.pos = 0;
     }
 
     void goEnd() {
-        pos = line.length() - 1;
+        //Mueve el cursor al final de la linea
+        this.pos = this.line.length() - 1;
     }
+
 }
