0e14d413f90ebab0ba9f2eca34fb45b9430845a0
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-05 19:16:16 +0200

    Update Line.java

--- Practica 1/MVC/Line.java
+++ Practica 1/MVC/Line.java
@@ -1,93 +1,157 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica1.MVC;
+package MVC;
 
+//import java.util.ArrayList;
+
+import Constants.Keyboard;
 import java.util.Observable;
 
-/**
- *
- * @author Marina
- */
 public class Line extends Observable {
-    
+
+    //private ArrayList<Character> linea;
     private String line;
     private int pos;
-    private boolean permission;
+    private boolean permission; //Variable utilizada para dar permiso a la hora de insertar un caracter
 
     public Line() {
-        line = new String();
-        pos = 0;
-        permission = false;
+        this.line = new String();
+        //this.linea = new ArrayList<>();
+        this.pos = 0;
+        this.permission = false;
     }
 
     public String getLine() {
-        return line;
+        //Devuelve el contenido de la linea
+        return this.line;
+        //return this.linea; en el caso de que hagamos un metodo ArrayList
+
     }
 
     public int getPos() {
-        return pos;
+        //Devuelve la posicion actual del cursor
+        return this.pos;
     }
 
-    public Boolean getPermission() {
-        return permission = true;
-    }
-    //Buffer,String,Lista...
-    /*public void insertChar(char character) { //Revisar
-        if (permission && pos != line.length()) {
-            line += Character.toString(character);
-        }else{
-            
-        }
-        pos++;
-        setChanged();
-        notifyObservers();
-    }*/
-
-    public void deleteChar() {//Revisar
-        if (pos < line.length()) {
-            line = line.substring(pos - 1, pos);
-        }
-        pos--;
-        setChanged();
-        notifyObservers();
+    public void insert() {
+        //Damos permiso para sobreescribir
+        this.permission = !this.permission;
+        this.setChanged();
+        this.notifyObservers(Keyboard.INS);
     }
 
-    public void bkspChar() {//Revisar
-        if (pos < line.length()) {
-            line = line.substring(pos, pos + 1);
+    public void insertChar(char character) {
+        //Miramos si tenemos permiso para sobreescribir
+        if (this.permission) {
+            if (this.pos < this.line.length()) {
+                //Si no tenemos el cursor al final de la linea insertamos en la posicion actual substituyendo por el caracter que habia antes
+                this.line = this.line.substring(0, this.pos) + Character.toString(character) + this.line.substring(this.pos + 1, this.line.length());
+            } else {
+                //Si estamos al final de la linea substituimos el caracter de la ultima posicion por el que insertamos
+                this.line = this.line.substring(0, this.line.length() - 1) + Character.toString(character);
+            }
+        } else {
+            //Si no tenemos permiso para sobreescribir insertamos el caracter en la posicion actual sin sobreescribir
+            this.line = this.line = this.line.substring(0, this.pos) + Character.toString(character) + this.line.substring(this.pos, this.line.length());
         }
-        setChanged();
-        notifyObservers();
+        /*
+        //Miramos si tenemos permiso para sobreescribir
+        if (this.permission) {
+            if (this.pos < this.linea.size()) {
+                //Si no tenemos el cursor al final de la linea insertamos en la posicion actual substituyendo por el caracter que habia antes
+                this.linea = this.linea.set(this.pos, character);
+            } else {
+                //Si estamos al final de la linea substituimos el caracter de la ultima posicion por el que insertamos
+                this.linea = this.linea.add(this.pos,character);
+            }
+        } else {
+            //Si no tenemos permiso para sobreescribir insertamos el caracter en la posicion actual sin sobreescribir
+            this.linea = this.linea.add(this.pos, character) ;
+        }*/
+        this.pos++;
+        this.setChanged();
+        //this.notifyObservers(this.pos); ?????????????????
+
+    }
+
+    public void deleteChar() {
+        //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        /*if (this.pos != this.linea.size()) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.linea = this.linea.remove(this.pos);
+        }*/
+
+        //Si no me encuentro al inicio de linea procedo a borrar
+        if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.line = this.line.substring(0, this.pos) + this.line.substring(this.pos + 1, this.line.length());
+
+        }
+        this.setChanged();
+        this.notifyObservers(Keyboard.DEL);
+    }
+
+    public void bkspChar() {
+        //Si //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        /*if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.linea = this.linea.remove(this.pos - 1);
+            this.goLeft();
+        }
+         */
+        //Si //Si no me encuentro al inicio de linea procedo a borrarno me encuentro al inicio de linea procedo a borrar
+        if (this.pos > 0) {
+            //Borramos el caracter de la posicion anterior del cursor
+            this.line = this.line.substring(0, this.pos - 1) + this.line.substring(this.pos, this.line.length());
+            this.goLeft();
+        }
+        this.setChanged();
+        this.notifyObservers(Keyboard.BKSP);
     }
 
     public void goLeft() {
-        if (pos != 0) {
-            pos--;
+        //Mueve el cursor a la izquierda
+        if (this.pos != 0) {
+            //Si el cursor no se encuentra al principio de la linea lo mueve a la izquierda
+            this.pos--;
         }
-        setChanged();
-        notifyObservers();
+        this.setChanged();
+        this.notifyObservers(Keyboard.LEFT);
     }
 
     public void goRight() {
-        if (pos != line.length()) {
-            pos++;
+        //Mueve el cursor a la derecha
+        if (this.pos != this.line.length()) { // this.linea.size() si es ArrayList
+            //Si el cursor no se encuentra al final de la linea lo mueve a la derecha
+            this.pos++;
         }
-        setChanged();
-        notifyObservers();
+        this.setChanged();
+        this.notifyObservers(Keyboard.RIGHT);
     }
 
     public void goHome() {
-        pos = 0;
-        setChanged();
-        notifyObservers();
+        //Mueve el cursor al inicio de la linea
+        this.pos = 0;
+        this.setChanged();
+        this.notifyObservers(Keyboard.HOME);
     }
 
     public void goEnd() {
-        pos = line.length() - 1;
-        setChanged();
-        notifyObservers();
+        //Mueve el cursor al final de la linea
+        this.pos = this.line.length() - 1;
+        //this.pos = this.linea.size() -1;
+        this.setChanged();
+        this.notifyObservers(Keyboard.END);
+    }
+
+    @Override
+    public String toString(){
+        return this.line;
+        /*
+        String vector = "";
+        for(Character character: this.linea){
+            vector += character
+        }
+        return vector;
+        */
+        
     }
 }
