a91bc7130a25fa3057fac86f7a7ccae5e9dc6ea8
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-05 19:14:32 +0200

    Update EditableBufferedReader.java

--- Practica 1/Scanner/EditableBufferedReader.java
+++ Practica 1/Scanner/EditableBufferedReader.java
@@ -1,76 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica1.Scanner;
+package Scanner;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Scanner;
 
-/**
- *
- * @author Marina
- */
 public class EditableBufferedReader extends BufferedReader {
-    
-    private static final int LEFT = 'D';
-    private static final int RIGHT = 'C';
-    private static final int HOME = '1';
-    private static final int END = '4';
-    private static final int INS = '2';
-    private static final int DEL = '3';
-    private static final int ESC = '\033';
-    private static final int CSI = '[';
+
     private Line line;
-    
+
     public EditableBufferedReader(Reader reader) {
         super(reader);
         line = new Line();
     }
-    
+
     public void setRaw() throws IOException, InterruptedException {
+        //pasar el terminal de modo cooked a modo raw
         try {
             String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
             Runtime.getRuntime().exec(cmd).waitFor();
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             System.out.println("Error al passar al mode Raw");
         }
     }
-    
+
     public void unsetRaw() throws InterruptedException {
+        //pasar el terminal de modo raw a modo cooked
         try {
             String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
             Runtime.getRuntime().exec(cmd).waitFor();
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             System.out.println("Error al passar al mode Cooked");
         }
     }
-    
+
     public int read() {
+        //Lee el caracter o tecla introducido utilizando la clase scanner
         Scanner reader = new Scanner(System.in);
+        //Guardamos el caracter en un entero
         int integer = reader.nextInt();
-        while(reader.hasNextInt()) {
+        //Mientras haya algun caracter mas a leer los va guardando y lo printa por pantalla
+        while (reader.hasNextInt()) {
             integer = reader.nextInt();
             System.out.println();
         }
-        
+
         return integer;
     }
-    
+
     public String readLine() {
+        //Lee la linea introducida con opcion de modificarla
         Scanner reader = new Scanner(System.in);
+        //Guardamos la linea en un string
         String sTexto = reader.nextLine();
-        while(reader.hasNextLine()) {
+        //Mientras haya caracteres para leer los va guardando y los printa por pantalla
+        while (reader.hasNextLine()) {
             sTexto = reader.nextLine();
             System.out.println();
         }
-        
+
         return sTexto;
     }
-    
+
 }
