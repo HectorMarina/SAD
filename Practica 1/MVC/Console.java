fe8d4d4ab5bee4eef82c84cb02d1b6724dce4650
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-09 10:42:02 +0200

    Update Console.java

--- Practica 1/MVC/Console.java
+++ Practica 1/MVC/Console.java
@@ -3,24 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practica1.MVC;
+package MVC;
 
+import Constants.Keyboard;
 import java.util.Observable;
 import java.util.Observer;
 
-/**
- *
- * @author Marina
- */
 public class Console implements Observer {
-    private Line line;
+
+    
 
     public Console(Line line) {
-        this.line = line;
+        
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int cmd = (int) arg;
+        switch (cmd) {
+            case Keyboard.LEFT:
+                System.out.println();
+                break;
+            case Keyboard.RIGHT:
+                System.out.println();
+                break;
+            case Keyboard.HOME:
+                System.out.println();
+                break;
+            case Keyboard.END:
+                System.out.println();
+                break;
+            case Keyboard.INS:
+                System.out.println();
+                break;
+            case Keyboard.BKSP:
+                System.out.println();
+                break;
+            
+        }
     }
 }
