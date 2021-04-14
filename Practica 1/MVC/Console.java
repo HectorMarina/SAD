4107c4500cdb106560544de5a3a58af66dc8e56c
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-14 17:01:49 +0200

    Add files via upload

--- Practica 1/MVC/Console.java
+++ Practica 1/MVC/Console.java
@@ -3,43 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package MVC;
+package practica1.MVC;
 
-import Constants.Keyboard;
 import java.util.Observable;
 import java.util.Observer;
+import practica1.Constants.Key;
+import practica1.Constants.Keyboard;
 
+/**
+ *
+ * @author Marina
+ */
 public class Console implements Observer {
-
-    
+    //private Line line;
 
     public Console(Line line) {
-        
+        //this.line = line;
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        
         int cmd = (int) arg;
         switch (cmd) {
             case Keyboard.LEFT:
-                System.out.println();
+                System.out.println(Key.LEFT);
                 break;
             case Keyboard.RIGHT:
-                System.out.println();
+                System.out.println(Key.RIGHT);
                 break;
             case Keyboard.HOME:
-                System.out.println();
+                System.out.println(Key.HOME);
                 break;
             case Keyboard.END:
-                System.out.println();
+                System.out.println(Key.END);
                 break;
             case Keyboard.INS:
-                System.out.println();
+                System.out.println(Key.INS);
                 break;
             case Keyboard.BKSP:
-                System.out.println();
+                System.out.println(Key.BKSP);
                 break;
-            
         }
     }
 }
