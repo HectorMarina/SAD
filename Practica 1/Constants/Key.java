a268ebe741b3d53cdae7d267e4f1a89554cd33ac
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-04-14 17:03:12 +0200

    Add files via upload

---
+++ Practica 1/Constants/Key.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1.Constants;
+
+/**
+ *
+ * @author Marina
+ */
+public abstract class Key {
+    
+    public static final int LEFT = 'D';
+    public static final int RIGHT = 'C';
+    public static final int HOME = '1';
+    public static final int END = '4';
+    public static final int INS = '2';
+    public static final int DEL = '3';
+    public static final int ESC = '\033';
+    public static final int BKSP = '\b';
+    public static final int CSI = '[';
+    //public static final int INT = '13';
+}
