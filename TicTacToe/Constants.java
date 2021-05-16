894a2ee4e8feb5711f7d684c8388e95cdd6381fa
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-05-16 13:18:29 +0200

    Update Constants.java

--- TicTacToe/Constants.java
+++ TicTacToe/Constants.java
@@ -1,26 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TicTacToe;
 
 import java.awt.Font;
 
-/**
- *
- * @author Marina
- */
 public class Constants {
     public static final int FILAS = 3;
     public static final int COLUMNAS = 3;
+    public static final int FILAS4 = 6;
+    public static final int COLUMNAS4 = 7;
     public static final int SQUARE = 75;
-    public static final int X1 = 94;
-    public static final int X2 = 189;
-    public static final int X3 = 287;
-    public static final int Y1 = 97;
-    public static final int Y2 = 195;
-    public static final int Y3 = 290;
+    public static final int SQUARE4 = 50;
+    public static final int X11 = 94;
+    public static final int X22 = 189;
+    public static final int X33 = 287;
+    public static final int Y11 = 97;
+    public static final int Y22 = 195;
+    public static final int Y33 = 290;
+    public static final int X1 = 100;
+    public static final int X2 = 150;
+    public static final int X3 = 200;
+    public static final int X4 = 250;
+    public static final int X5 = 300;
+    public static final int X6 = 350;
+    public static final int X7 = 400;
+    public static final int Y1 = 70;
+    public static final int Y2 = 120;
+    public static final int Y3 = 170;
+    public static final int Y4 = 220;
+    public static final int Y5 = 270;
+    public static final int Y6 = 320;
+    public static final int Y7 = 370;
     public static final Font LETRAMENUOPCIONES = new Font("arial", Font.BOLD, 12);
     public static final Font LETRANOMBRES = new Font("arial", Font.BOLD, 20);
     public static final int CASILLAVACIA = 0;
