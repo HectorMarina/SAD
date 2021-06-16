9d89629b6da4706a83591483a933b8738395da27
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-06-16 13:08:03 +0200

    Update Server.java

--- Practica 2/Server.java
+++ Practica 2/Server.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Server implements Runnable {
-
+    //Para el server utilizaremos un concurrentMap que es más eficiente que las otras opciones
     public static ConcurrentHashMap<String, MySocket> users = new ConcurrentHashMap<>();
     public static final int PORT = 5000;
     MySocket mySocket;
@@ -17,25 +17,28 @@ public class Server implements Runnable {
     }
 
     public static void main(String[] args) throws IOException {
+        //Creamos el socket
         MyServerSocket myServerSocket = new MyServerSocket(PORT);
         System.out.println("[Server] Please wait while the server is starting...");
 
         while (true) {
+            //Aceptamos el socket para que se pueda usar
             MySocket socket = myServerSocket.accept();
             while (!nextUser) {
                 
                 socket.println("Please introduce your username: ");
                 String username = socket.readLine();
-                
+                //Leemos el nombre del usuario y comprobamos que no esté ya en uso
                 if (users.containsKey(username)) {
                     
                     socket.println("The username is not available, please introduce another: ");
-                    
+                    //Si ya existe perdimos otra vez el nombre
                 } else {
                     socket.println("The user:  " +username+ " is connected");
                     users.put(username, socket);
                     new Thread(new Server(username, socket)).start();
                     nextUser = true;
+                    //Si no existe lo creamos
                 }
             }
             nextUser = false;
@@ -59,7 +62,9 @@ public class Server implements Runnable {
     }
 
     private boolean connection(String nickname, String line) {
+        //Comprobamos el estado del usuario
         boolean connection = true;
+        //Comprobamos si quieres salir y lo eliminamos del map
         if(line.equals("exit")){
             users.remove(nickname);
             
@@ -69,6 +74,7 @@ public class Server implements Runnable {
                     socket.println(nickname + " has left");
                     System.out.println(nickname + " says goodbye!!");
                     System.out.println(nickname + " is disconnected!!");
+                    socket.close();
                 }
             }
             connection = false;
