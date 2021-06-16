5b84b928e8c11cdd561596ac5c942d59e663b1ab
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-06-16 13:07:08 +0200

    Update MyServerSocket.java

--- Practica 2/MyServerSocket.java
+++ Practica 2/MyServerSocket.java
@@ -2,7 +2,6 @@ package prac2;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,11 +10,13 @@ public class MyServerSocket extends ServerSocket {
     private MySocket socket;
 
     public MyServerSocket(int port) throws IOException {
+        //Creamos el socket y lo enlazamos al puerto
         super(port);
     }
 
     @Override
     public MySocket accept() throws IOException {
+        //Espera a que haya una conexión, entonces crea y devulelve el socket 
         try {
              this.socket = new MySocket(super.accept());
            
@@ -28,6 +29,7 @@ public class MyServerSocket extends ServerSocket {
     @Override
     public void close(){
         try{
+            //Cierra la conexión del socket
             super.close();
         }catch(IOException e){
             Logger.getLogger(MyServerSocket.class.getName()).log(Level.SEVERE,null,e);
