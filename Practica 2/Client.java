6f4f777b804fcf3849a026e178ddf0e936259dc3
HectorMarina <79509349+HectorMarina@users.noreply.github.com>
2021-06-16 13:06:20 +0200

    Update Client.java

--- Practica 2/Client.java
+++ Practica 2/Client.java
@@ -3,8 +3,7 @@ package prac2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-import oracle.jrockit.jfr.VMJFR;
+
 
 public class Client {
 
@@ -15,7 +14,7 @@ public class Client {
     public static void main(String[] args) throws IOException {
         BufferedReader entry = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("[Client] Please introduce your nickname\n");
-
+        //Pedimos el nickname del usuario
         try {
             nickname = entry.readLine();
             System.out.println("[Client] Hello " + nickname + " now you are connected!\n");
@@ -23,7 +22,7 @@ public class Client {
         } catch (IOException e) {
             System.out.println("[Error] Couldn't read the message!!");
         }
-
+        //Creamos el socket
         MySocket socket = new MySocket(HOST, PORT);
 
         //Input Thread
